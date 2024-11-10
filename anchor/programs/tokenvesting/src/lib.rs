eb6084bbf8e31ae0b415d9f7663e08adb7d51798
sayandas-sd <sayandasmac@gmail.com>
2024-11-10 18:42:39 +0530

    initial commit

---
+++ anchor/programs/tokenvesting/src/lib.rs
@@ -0,0 +1,14 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
+
+#[program]
+pub mod tokenvesting {
+    use super::*;
+
+    
+  
+}
+
