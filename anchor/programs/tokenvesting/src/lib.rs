d5f40935e76d124ca70754093ed02a563de86cdc
sayandas-sd <sayandasmac@gmail.com>
2024-11-10 22:52:47 +0530

    created claim token function

--- anchor/programs/tokenvesting/src/lib.rs
+++ anchor/programs/tokenvesting/src/lib.rs
@@ -42,6 +42,11 @@ pub mod tokenvesting {
       Ok(())
     }
 
+
+    pub fn claim_tokens(ctx: Contex<ClaimTokens>, company_name: String) -> Result<()> {
+      Ok(())
+    }
+
   
 }
 
@@ -81,6 +86,8 @@ pub struct CreatingVestingAccount<'info> {
   pub token_program: Interface<'info, TokenInterface>,
 }
 
+
+
 #[deirve(Accounts)]
 pub struct CreatingEmployeeAccount<'info> {
   #[account(mut)]
@@ -101,6 +108,55 @@ pub struct CreatingEmployeeAccount<'info> {
 
   pub employee_account: Account<'info, EmployeeAccount>, 
 
+  pub system_program: Program<'info, System>,
+
+}
+
+#[derive(Accounts)]
+#[instruction(company_name: String)]
+pub struct ClaimTokens<'info> {
+  #[account(mut)]
+
+  pub beneficiary: Signer<'info>,
+
+  #[acccount(
+    mut,
+    seeds = [b"employee_vesting", beneficiary.key().as_ref(), vesting_account.key().as_ref()],
+    bump = employee_account.bump,
+    has_one = beneficiary,
+    has_one = vesting_account,
+
+  )]
+
+  pub employee_account: Accounts<'info, EmployeeAccount>,
+
+  //check correct beneficiary or employee can claim the token
+
+  #[acccount(
+    mut,
+    seeds = [company_name.as_bytes()],
+    bump = vesting_account.bump,
+    has_one = treasury_token_account,
+    has_one = mint,
+  )]
+
+  pub vesting_account:  Account<'info, VestingAccount>,
+
+  pub mint: InterfaceAccount<'info, Mint>,
+
+  #[account(mut)]
+  pub treasury_token_account: InterfaceAccount<'info, TokenAccount>,
+
+  #[acccount(mut)]
+  init_if_needed,
+  payer = beneficiary,
+  associated_token::Mint = mint,
+  associated_token::authority = beneficiary,
+  associated_token::token_program = token_program,
+
+  pub employee_token_account: InterfaceAccount<'info, TokenAccount>,
+  pub token_program: Interface<'info, TokenInterface>,
+  pub associated_token_program: Program<'info, AssociatedToken>,
   pub system_program: Program<'info, System>,
 
 }
@@ -128,5 +184,6 @@ pub struct EmployeeAccount {
   pub vesting_account: PubKey,
   pub total_amount: u64,
   pub total_withdrawn: u64,
+  pub bump: u8
 }
 
