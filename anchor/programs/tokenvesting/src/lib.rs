e06fa71d99ac8e9731fb300aaf0cddca33a02a53
sayandas-sd <sayandasmac@gmail.com>
2024-11-10 23:18:44 +0530

    added claim token amount check and time check

--- anchor/programs/tokenvesting/src/lib.rs
+++ anchor/programs/tokenvesting/src/lib.rs
@@ -53,6 +53,32 @@ pub mod tokenvesting {
         return Err(ErrorCode::ClaimNotAvailableYet.into())
       } 
 
+      let time_since_start = now.saturating_sub(employee_account.start_time);
+      let total_vesting_time = employee_account.end_time.saturating_sub(employee_account.start_time);
+
+      if total_vesting_time == 0 {
+        return Err(ErrorCode::InvalidVestingPeriod.into())
+      }
+
+      let vested_account = if now >= employee_account.end_time {
+        employee_account.total_amount
+      } else {
+        match employee_account.total_amount.checked_mul(time_since_start as u64) {
+          Some(product) => (
+            product / total_vesting_time as u64
+          ),
+          None => (
+            return Err(ErrorCode::CalculationOverFlow.into())
+          )
+        }
+      }
+
+      let claimable_amount = vested_account.saturating_sub(employee_account.total_withdrawn);
+      
+      if claimable_amount == 0 {
+        return Err(ErrorCode::NothingClaim.into()) 
+      }
+
       Ok(())
     }
 
@@ -201,4 +227,10 @@ pub struct EmployeeAccount {
 pub enum ErrorCode {
   #[msg("Sorry Claim not Available yet")]
   ClaimNotAvailableYet
+  #[msg("Invalid Vesting Period")]
+  InvalidVestingPeriod
+  #[msg("calcualtion overflow")]
+  CalculationOverFlow
+  #[msg("Nothing is Claim")]
+  NothingClaim
 }
